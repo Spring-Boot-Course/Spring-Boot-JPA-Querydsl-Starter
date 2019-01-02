20baa2070284695346e6ede8d9a8b590517d6228
SML <dltkdals2202@gmail.com>
2019-01-02 19:12:31 +0900

    feat : test 코드 작성

--- src/test/java/me/sml/springjpaquerydsl/ApplicationTests.java
+++ src/test/java/me/sml/springjpaquerydsl/ApplicationTests.java
@@ -1,16 +1,48 @@
 package me.sml.springjpaquerydsl;
 
+import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationTests {
 
+    @Autowired
+    private AcademyRepository academyRepository;
+
+    @Autowired
+    private AcademyRepositorySupport academyRepositorySupport;
+
+    @After
+    public void tearDown() throws Exception{
+        academyRepository.deleteAllInBatch();
+    }
+
     @Test
-    public void contextLoads() {
+    public void querydsl_기본_기능_확인(){
+        //given
+        String name = "sml";
+        String address = "test@test.com";
+        academyRepository.save(Academy.builder()
+                                .name(name)
+                                .address(address)
+                                .build());
+
+        //when
+        List<Academy> result = academyRepositorySupport.findByName(name);
+
+        //then
+        assertThat(result.size(), is(1));
+        assertThat(result.get(0).getAddress(), is(address));
     }
 
 }
